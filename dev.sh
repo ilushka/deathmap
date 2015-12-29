794f7848c5cf51e71dd5626b7c9dc225fb8cf9c6
ilya moskovko <ilya@iswifter.net>
2015-12-28 23:31:10 -0800

    ** added help to dev.sh script **

--- dev.sh
+++ dev.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 
-function heroku_commands() {
+set -e
+
+BIN_NAME=$(basename $0)
+
+show_usage() {
     case $1 in
-        "push")
-            git push heroku master
+        "heroku")
+            printf "Usage: ${BIN_NAME} heroku <"
+            cat ${BIN_NAME} | awk 'BEGIN { FS = "\""; ORS = "|"; } /\"[a-z]+\"\) # heroku-arg/ { print $2; }'
+            printf ">\n"
             ;;
-        "dbpush")
-            heroku pg:push postgresql://deathmap:password@localhost/deathmap DATABASE_URL
+        *)
+            printf "Usage: ${BIN_NAME} <"
+            cat ${BIN_NAME} | awk 'BEGIN { FS = "\""; ORS = "|"; } /\"[a-z]+\"\) # first-level-arg/ { print $2; }'
+            printf ">\n"
             ;;
     esac
+}
+
+show_readme() {
+    printf "
+## Quick Start
+"
 }
 
 function git_push() {
@@ -31,18 +45,40 @@ function start_local() {
         python deathmap.py
 }
 
+function heroku_commands() {
+    case $1 in
+        "push") # heroku-arg;
+            git push heroku master
+            ;;
+        "dbpush") # heroku-arg;
+            heroku pg:push postgresql://deathmap:password@localhost/deathmap DATABASE_URL
+            ;;
+        "dbpull") # heroku-arg;
+            heroku pg:pull DATABASE_URL postgresql://deathmap:password@localhost/deathmap
+            ;;
+    esac
+}
+
 case $1 in
-    "heroku")
+    "heroku") # first-level-arg;
         heroku_commands "${@:2}"
         ;;
-    "push")
+    "push") # first-level-arg;
         git_push "${@:2}"
         ;;
-    "status")
+    "status") # first-level-arg;
         status
         ;;
-    "start")
+    "start") # first-level-arg;
         start_local
         ;;
+    "help") # first-level-arg;
+        show_usage "${@:2}"
+        ;;
+    "readme") # first-level-arg;
+        show_readme
+        ;;
+    *)
+        show_usage
+        ;;
 esac
-
