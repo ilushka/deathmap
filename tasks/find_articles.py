84fe13e97bf4d74565b88b5deae44efb43d81277
ilya moskovko <ilya@iswifter.net>
2016-12-12 23:29:25 -0800

    ** removed erroneous ident **

--- tasks/find_articles.py
+++ tasks/find_articles.py
@@ -6,17 +6,18 @@ TWITTER_ACCOUNTS = ["mercnews"]
 
 with app.app_context():
   for acc_name in TWITTER_ACCOUNTS:
+    # prepare twitter feed
     feed = TwitterFeed("mercnews")
-    dbmarker = db.session.query(FeedMarker)
-                    .filter_by(FeedMarker.feed_name=feed.get_feed_name())
-                    .first()
+    dbmarker = db.session.query(FeedMarker).filter_by(FeedMarker.feed_name=feed.get_feed_name()).first()
     feed.set_last_post_marker(dbmarker.marker)
 
+    # process all the post since marker
     posts = feed.get_posts()
     for p in posts:
       article = Article(p.title, p.link)
       db.session.add(article)
 
+    # store new marker
     marker = feed.get_last_post_marker()
     if marker is not None:
       print marker
