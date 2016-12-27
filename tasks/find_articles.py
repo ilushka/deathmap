bb7e12bfe3f0a35fca60f25445cd9f8e6534e0d2
ilya moskovko <ilya@iswifter.net>
2016-12-27 14:21:49 -0800

    ** added debug prints to find_articles.py **

--- tasks/find_articles.py
+++ tasks/find_articles.py
@@ -3,6 +3,7 @@ from deathdb import Article, FeedMarker
 from twitterfeed import TwitterFeed, TwitterPost
 
 TWITTER_ACCOUNTS = ["mercnews"]
+DEBUG = True
 
 with app.app_context():
   for acc_name in TWITTER_ACCOUNTS:
@@ -16,18 +17,23 @@ with app.app_context():
     # process all the post since marker
     posts = feed.get_posts()
     for p in posts:
+      print("MONKEY: " + p.title + " " + p.link)
       article = Article(p.title, p.link)
-      db.session.add(article)
+      if not DEBUG:
+        db.session.add(article)
 
     # store new marker
     marker = feed.get_last_post_marker()
     if marker is not None:
-      print marker
-      if dbmarker is None:
-        dbmarker = FeedMarker(feed.get_feed_name(), feed.get_last_post_marker())
-        db.session.add(dbmarker)     
-      else:
-        dbmarker.marker = feed.get_last_post_marker()
+      print("MONKEY: feed_name: " + feed.get_feed_name())
+      print("MONKEY: marker: " + marker)
+      if not DEBUG:
+        if dbmarker is None:
+          dbmarker = FeedMarker(feed.get_feed_name(), feed.get_last_post_marker())
+          db.session.add(dbmarker)     
+        else:
+          dbmarker.marker = feed.get_last_post_marker()
 
-    db.session.commit()
+    if not DEBUG:
+      db.session.commit()
 
