cb8ab9cea1c8ac2ffa514de3161e9f81bd077ada
ilya moskovko <ilya@iswifter.net>
2016-12-28 21:16:23 -0800

    ** added sfgate and eastbaytimes **

--- tasks/find_articles.py
+++ tasks/find_articles.py
@@ -2,13 +2,13 @@ from deathmap import db, app
 from deathdb import Article, FeedMarker
 from twitterfeed import TwitterFeed, TwitterPost
 
-TWITTER_ACCOUNTS = ["mercnews"]
+TWITTER_ACCOUNTS = ["mercnews", "sfgate", "eastbaytimes"]
 DEBUG = False
 
 with app.app_context():
   for acc_name in TWITTER_ACCOUNTS:
     # prepare twitter feed
-    feed = TwitterFeed("mercnews")
+    feed = TwitterFeed(acc_name)
     # retrieve stored marker in db
     dbmarker = db.session.query(FeedMarker).filter_by(feed_name=feed.get_feed_name()).first()
     if dbmarker is not None:
@@ -17,7 +17,6 @@ with app.app_context():
     # process all the post since marker
     posts = reversed(feed.get_posts())  # add oldest posts first
     for p in posts:
-      print("MONKEY: " + p.title + " " + p.link)
       article = Article(p.title, p.link)
       if not DEBUG:
         db.session.add(article)
@@ -25,8 +24,6 @@ with app.app_context():
     # store new marker
     marker = feed.get_last_post_marker()
     if marker is not None:
-      print("MONKEY: feed_name: " + feed.get_feed_name())
-      print("MONKEY: marker: " + marker)
       if not DEBUG:
         if dbmarker is None:
           dbmarker = FeedMarker(feed.get_feed_name(), feed.get_last_post_marker())
